6eef6c93fdff044cb5d45b0f8e7e24530dc83600
zschmitt3 <zschmitt3@my.wctc.edu>
2023-02-13 14:05:48 -0600

    Wow. I started the homework.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using NLog;
+
+System.Console.WriteLine("Enter 1 to print movies");
+System.Console.WriteLine("Enter 2 to add a movie");
+System.Console.WriteLine("Enter anything else to quit");
+char input = char.Parse(Console.ReadLine());
+var logger = LogManager.LoadConfiguration(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
+if (input == '1'){
+    logger.Info("Print selected.");
+    
+}else if (input == '2'){
+
+}

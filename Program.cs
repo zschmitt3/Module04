a7377e15530df038158bb22f405e6f22be516a08
zschmitt3 <zschmitt3@my.wctc.edu>
2023-02-13 14:47:12 -0600

    Still broken

--- Program.cs
+++ Program.cs
@@ -5,8 +5,8 @@ System.Console.WriteLine("Enter 2 to add a movie");
 System.Console.WriteLine("Enter anything else to quit");
 char input = char.Parse(Console.ReadLine());
 var logger = LogManager.LoadConfiguration(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
-StreamReader sr = new StreamReader("movies.csv");
 StreamWriter sw = new StreamWriter("movies.csv");
+StreamReader sr = new StreamReader("movies.csv");
 
 
 

84323f62c65984666eb9c63cba8fe1cb23ae6a59
zschmitt3 <zschmitt3@my.wctc.edu>
2023-02-13 14:44:42 -0600

    It breaks at line 9

--- Program.cs
+++ Program.cs
@@ -5,9 +5,47 @@ System.Console.WriteLine("Enter 2 to add a movie");
 System.Console.WriteLine("Enter anything else to quit");
 char input = char.Parse(Console.ReadLine());
 var logger = LogManager.LoadConfiguration(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
+StreamReader sr = new StreamReader("movies.csv");
+StreamWriter sw = new StreamWriter("movies.csv");
+
+
+
 if (input == '1'){
     logger.Info("Print selected.");
-    
+    string dataLine = sr.ReadLine();
+    string movie = dataLine.Substring(dataLine.IndexOf(",")+1,dataLine.LastIndexOf(",")-7);
+    while (dataLine != null){
+        try{
+            dataLine = sr.ReadLine();
+            movie = dataLine.Substring(dataLine.IndexOf(",")+1,dataLine.LastIndexOf(",")-7);
+            System.Console.WriteLine(movie);
+        }catch{
+            dataLine = null;
+        }
+    }
+    logger.Info("Print completed");
 }else if (input == '2'){
-
+    logger.Info("Add selected");
+    string newMovie = Console.ReadLine();
+    string dataLine = ","+sr.ReadLine()+",FillerTextFilling";
+    string movie = dataLine.Substring(dataLine.IndexOf(",")+1,dataLine.LastIndexOf(",")-7);
+    bool isNew = true;
+    while (dataLine != null){
+        try{
+            dataLine = ","+sr.ReadLine()+",FillerTextFilling";
+            movie = dataLine.Substring(dataLine.IndexOf(",")+1,dataLine.LastIndexOf(",")-7);
+            if (movie == newMovie){
+                logger.Info("Duplicate movie");
+                System.Console.WriteLine("That movie is already in the file.");
+                isNew = false;
+            }
+        }catch{
+            dataLine = null;
+        }
+    }
+    if (isNew){
+        sw.WriteLine(newMovie);
+        System.Console.WriteLine("Movie added successfully");
+        logger.Info("Movie added");
+    }
 }
